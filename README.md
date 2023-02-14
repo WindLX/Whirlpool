4bbc95224ff5b5c130f7ec483828638f527209c8
wind <1418043337@qq.com>
2023-02-14 22:09:17 +0800

    update: settings and README

--- README.md
+++ README.md
@@ -12,4 +12,28 @@ A command-line tool for converting **png ico jpg** format files into **png ico j
 
 ### How to Use
 
-1. Just run wht.ps1 in your PowerShell
+1. Make sure you have **Python** and **Powershell** on your computer.
+
+2. Please set the **WhirlpoolPath** in **settings.json** to the absolute path of your Whirlpool, the default path is *"D:\\\\Whirlpool"* .
+
+3. Run **wht.ps1** in your PowerShell.
+
+4. This script automatically configures Python's virtual environment in the Whirlpool directory if you are using it for the first time and downloads the required libraries using pip.
+
+5. The Powershell script has five parameters:
+
+    + **-Path** The path of folder which storages your target pictures.
+    + **-IsHidden** Change the presentation form of work progress.
+    + **-Size** The size of output pictures, only effective when target type is ico.
+    + **-RawType** The raw type of pictures, such as png jpg ico.
+    + **-TargetType** The target type of pictures, such as png jpg ico.
+
+    You can use `Get-Help .\wht.ps1` for details.
+
+6. An example is
+
+    ```powe
+    .\wht.ps1 . 0 128 png ico
+    ```
+
+7. The result will be stored in *".\whirlpool_result"*.
--- wht.ps1
+++ wht.ps1
@@ -8,11 +8,11 @@ The path of folder which storages your target pictures.
 .Parameter IsHidden
 Change the presentation form of work progress.
 .Parameter Size
-The size of output pictures, only effective when target type is ico 
+The size of output pictures, only effective when target type is ico. 
 .Parameter RawType
-The raw type of pictures, such as png jpg ico 
+The raw type of pictures, such as png jpg ico.
 .Parameter TargetType
-The target type of pictures, such as png jpg ico 
+The target type of pictures, such as png jpg ico.
 .Example
 .\wht.ps1 -Path .\Download -IsHidden true -Size 128
 #>
@@ -28,16 +28,28 @@ Param(
     [string] $TargetType
 )
 
-$PYTHON_ENV = ".\whirlpool"
-$PIP_REQUIREMENTS = "requirements.txt"
-$IS_EXIST = (Test-Path $PYTHON_ENV)
+$CONFIG = Get-Content "settings.json" | ConvertFrom-Json
+$WHIRLPOOL_PATH = $CONFIG.WhirlpoolPath
+$WHIRLPOOL_ENV = Join-Path $WHIRLPOOL_PATH "whirlpool"
+$ENV_ACTIVATE = Join-Path $WHIRLPOOL_ENV "Scripts\Activate.ps1"
+$WHIRLPOOL_START = Join-Path $WHIRLPOOL_PATH "whirlpool_terminal.py"
+$PIP_REQUIREMENTS = Join-Path $WHIRLPOOL_PATH "requirements.txt"
+$IS_EXIST = (Test-Path $WHIRLPOOL_ENV)
+$IS_EXIST_PILLOW = Join-Path $WHIRLPOOL_ENV "Lib\site-packages\Pillow*" | Test-Path
 if ($IS_EXIST) {}
 else {
-    python -m venv $PYTHON_ENV
-    .\whirlpool\Scripts\Activate.ps1
+    Write-Host "[INFO]" -NoNewline -ForegroundColor:Blue
+    Write-Host " MAKE PYTHON ENVIRONMENT" -ForegroundColor:Green
+    python -m venv $WHIRLPOOL_ENV
+}
+if ($IS_EXIST_PILLOW) {}
+else {
+    Invoke-Expression $ENV_ACTIVATE
+    Write-Host "[INFO]" -NoNewline -ForegroundColor:Blue
+    Write-Host " INSTALL PYTHON PACKAGES" -ForegroundColor:Green
     pip install -r $PIP_REQUIREMENTS
     deactivate
 }
-.\whirlpool\Scripts\Activate.ps1
-python whirlpool_terminal.py $Path $IsHidden $Size $RawType $TargetType
+Invoke-Expression $ENV_ACTIVATE
+python $WHIRLPOOL_START $Path $IsHidden $Size $RawType $TargetType
 deactivate
